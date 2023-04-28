9c1cb3f25e9f0aaa7dc33ed25affecd2e2a4d6e6
linhbtgch210465 <130551939+linhbtgch210465@users.noreply.github.com>
2023-04-29 00:43:35 +0700

    Update CartService.php

--- asm1103/app/Http/Services/CartService.php
+++ asm1103/app/Http/Services/CartService.php
@@ -14,8 +14,6 @@ class CartService
 {
     public function create($request)
     {
-        $qty = (int)$request->input('num_product');
-        $product_id = (int)$request->input('product_id');
 
         if ($qty <= 0 || $product_id <= 0) {
             Session::flash('error', 'Incorrect Quantity or Product');
