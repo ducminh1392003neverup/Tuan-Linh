8f50d6768c9c40f2ac9927edccfc9d8707e74cde
Trun6123 <130743352+Trun6123@users.noreply.github.com>
2023-04-29 01:14:32 +0700

    Update Product.php

--- asm1103/app/Models/Product.php
+++ asm1103/app/Models/Product.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $table = "products";
+    protected $primaryKey = "id";
+    protected $guarded = [];
+    // protected $fillable = [
+    //     'name',
+    //     'description',
+    //     'content',
+    //     'menu_id',
+    //     'price',
+    //     'price_sale',
+    //     'thumb'
+    // ];
+
+    public function menu()
+    {
+        return $this->hasOne(Menu::class, 'id', 'menu_id')
+            ->withDefault(['name' => '']);
+    }
 }
